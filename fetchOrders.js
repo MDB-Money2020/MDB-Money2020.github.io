cb864d87729b7a89d55e12c976a5e74f7fac5e24
Krishnan Rajiyah <krajiyah@gmail.com>
2017-10-21 21:13:15 -0700

    setup fetcher

---
+++ fetchOrders.js
@@ -0,0 +1,16 @@
+function everyMinuteFetchOrder(callback) {
+  setInterval(function() {
+    $.ajax({
+			url: "http://money2020-app.herokuapp.com/orders?restaurantId=-Kx0rrVSISYN1ZmefG8_",
+			type: "get",
+			contentType: "application/json",
+			dataType: "json",
+			success: function(response) {
+				callback(response.result);
+			},
+			error: function(error) {
+        console.error(error);
+			}
+		});
+  }, 1000);
+}
