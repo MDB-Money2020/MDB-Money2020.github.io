4d4f307c7e73b6be2a6db50b0e7060ec2936c3a0
Daniel Andrews <danielandrewsre@gmail.com>
2017-10-22 12:29:26 -0700

    already pitched, but the webapp is littt

--- fetchOrders.js
+++ fetchOrders.js
@@ -1,16 +1,31 @@
 function everyMinuteFetchOrder(callback) {
-  setInterval(function() {
+    setInterval(function() {
+        $.ajax({
+            url: "https://money2020-app.herokuapp.com/orders?restaurantId=-Kx0rrVSISYN1ZmefG8_",
+            type: "get",
+            contentType: "application/json",
+            dataType: "json",
+            success: function(response) {
+                callback(response.result);
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
+    }, 10000);
+}
+
+function getMenuItem(menuItemId, callback) {
     $.ajax({
-			url: "https://money2020-app.herokuapp.com/orders?restaurantId=-Kx0rrVSISYN1ZmefG8_",
-			type: "get",
-			contentType: "application/json",
-			dataType: "json",
-			success: function(response) {
-				callback(response.result);
-			},
-			error: function(error) {
-        console.error(error);
-			}
-		});
-  }, 1000);
+        url: "https://money2020-app.herokuapp.com/menuItems?menuItemId=" + menuItemId,
+        type: "get",
+        contentType: "application/json",
+        dataType: "json",
+        success: function(response) {
+            callback(response.result);
+        },
+        error: function(error) {
+            // console.error(error);
+        }
+    });
 }
